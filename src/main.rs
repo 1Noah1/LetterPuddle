efa85e986c3716f47ed83ea3369f487245cd7858
noah.storck <noah.storck@wetteronline.de>
2025-02-12 16:39:24 +0100

    remove input thread code

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,6 @@ use std::thread::{self};
 // use std::io::{self};
 
 fn main() {
-  //  let handle = thread::spawn(|| {
-  //      // Enable raw mode so that key events can be captured without pressing enter
-  //      let _stdout = io::stdout().into_raw_mode().unwrap();
-  //      // Create a handle for standard input (stdin)
-  //      let mut  stdin = io::stdin().lock().keys();
-  //
-  //      // this causes shift in letters
-  //      // loop {
-  //          // if let Some(Ok(Key::Esc)) = stdin.next() {
-  //              // panic!("Esc key pressed. Exiting loop.");
-  //          // }
-  //          // thread::sleep(time::Duration::from_millis(10000));
-  //      // }
-  //  });
-
     let mut manager = MapManager::new();
     // render and calculation
     let mut i = 0;
@@ -44,11 +29,4 @@ fn main() {
         }
         i += 1;
     }
-        
-
-
-    //MapManager::write_borders(&mut manager.map);
-
-    // Wait for the thread to finish
-   // handle.join().unwrap();
 }
