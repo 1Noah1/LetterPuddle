6352a8c5bf6f5e2a85d8b2903a044293b4f6bdbd
noah.storck <noah.storck@wetteronline.de>
2025-02-18 20:20:39 +0100

    little clean up

--- src/letter_service.rs
+++ src/letter_service.rs
@@ -5,9 +5,6 @@ pub struct LetterService;
 
 impl LetterService {
     pub fn get_letter(found_letters: &Vec<char>) -> char {
-        // TODO
-        // this algorithm is absolute asssss. im pretty sure it is not based on it's surrounding letters at the moment
-        // creates base probabilities
         let mut probabilities: HashMap<char, f32> = HashMap::new();
         for letter in 'A'..='Z' {
             probabilities.insert(letter, 0.1);
@@ -24,8 +21,7 @@ impl LetterService {
         let rnd_num = rand::rng().random_range(0.0..total_probability);
         let mut cumulative_probability: f32 = 0.0;
 
-        // this is omega stupid, i should be able to just return whithin the loop itself
-        // TODO: OPTIMIZE PLEASE
+        // TODO:(fix) cant return directly from the loop for some reason
         let mut final_letter = ' ';
         for (l, p) in probabilities {
             cumulative_probability += p;
--- src/main.rs
+++ src/main.rs
@@ -13,25 +13,17 @@ use std::{
     time::Instant,
 };
 
-//use termion::cursor;
-// use termion::{input::TermRead, event::Key, raw::IntoRawMode};
-// use std::io::{self};
-
 fn main() {
     let mut manager = MapManager::new();
-    // render and calculation
+
     let mut i = 0;
-
-    thread::sleep(time::Duration::from_millis(0));
-
     MapManager::init(&mut manager);
     let start = Instant::now();
     loop {
         let start = Instant::now();
         MapManager::draw_map(&mut manager.map);
         MapManager::grow(&mut manager);
-        //println!("main i: {}", i);
-        //thread::sleep(time::Duration::from_millis(100));
+        thread::sleep(time::Duration::from_millis(100));
         let end = Instant::now();
         println!("time: {:?}", end.duration_since(start));
         if i == 80 {
